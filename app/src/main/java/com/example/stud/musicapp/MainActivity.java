df848ebb3ce68567ad75372cffae3a07bc9ed200
W57129 <w57129@student.wsiz.rzeszow.pl>
2018-05-12 12:47:57 +0200

    aktywnoś TopSongs

--- app/src/main/java/com/example/stud/musicapp/MainActivity.java
+++ app/src/main/java/com/example/stud/musicapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.stud.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.example.stud.musicapp.topsongs.TopSongsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +21,10 @@ public class MainActivity extends AppCompatActivity {
         bTopSongs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "TODO", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "TODO", Toast.LENGTH_SHORT).show();
+                //uruchamianie layoutu
+                Intent intent = new Intent(MainActivity.this, TopSongsActivity.class);
+                startActivity(intent);
             }
         });
 
