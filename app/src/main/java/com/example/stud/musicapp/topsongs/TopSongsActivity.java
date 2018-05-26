b772d5e67b9303d816688041bb2917fee30fe9aa
w57129@student.wsiz.rzeszow.pl <1Stanek!>
2018-05-26 13:35:55 +0200

    TopSongsAdapter

--- app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.stud.musicapp.R;
@@ -17,13 +18,15 @@ import retrofit2.Response;
 
 public class TopSongsActivity extends AppCompatActivity {
 
-
+    RecycleView rvList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_songs);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        rvList = findViewById(R.id.rvList);
+        
         Call<TrendingList> trendingListCall = ApiService. getService ().getTrendingList( "us" ,
                 "itunes" , "singles" );
         trendingListCall.enqueue( new Callback<TrendingList>() {
