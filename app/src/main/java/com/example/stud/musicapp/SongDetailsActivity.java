a18efd30585b1b256605debb4bf810655e614b03
W57129 <w57129@student.wsiz.rzeszow.pl>
2018-06-23 12:25:06 +0200

    lab15 - menu przycisk ULUBIONE

--- app/src/main/java/com/example/stud/musicapp/SongDetailsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/SongDetailsActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -75,5 +77,12 @@ public class SongDetailsActivity extends AppCompatActivity {
             ImageView ivThumb = findViewById(R.id. ivThumb );
             Glide. with ( this ).load(track. strTrackThumb ).into(ivThumb);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu. favorite_menu , menu);
+        return true ;
     }
 }
